e62af629bf473aef8869806cfba5ea469fffd6d4
YJSNPIDISK <56146987+YJSNPIDISK@users.noreply.github.com>
2021-02-13 17:17:27 +0900

    commit on 'master'

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <hello></hello>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  components: {
+    hello: HelloWorld
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <button v-on:click="getData">가져오기</button>
+    <div>{{users}}</div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  el: '#app',
+  data:function(){return {users:[]}},
+  methods: {
+    getData: function() {
+      var vue = this;
+      axios.get('http://sc0nep.iptime.org:8000/search?word=다빈치코드&mode=0')
+        .then(function(response) {
+          console.log(response.data);
+          vue.users = response.data;
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
